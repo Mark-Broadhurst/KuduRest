1f587d23a39583b15be78a4835bc5b3fd47a2f3f
Mark Broadhurst <Mark.Broadhurst@gmail.com>
2017-02-08 16:56:03 +0000

    Doesnt work but need to stash

---
+++ KuduRest.fs
@@ -0,0 +1,45 @@
+﻿namespace Fake.Azure
+
+open Fake.Azure.KuduRest
+
+type KuduRestClient (siteName:string, username:string, password:string) =
+    
+    let baseUrl = Url.siteRoot siteName
+    let token = Http.CreateToken username password
+
+    member __.ScmInfo = Url.scmInfo baseUrl token ""
+    member __.ScmClean = Url.scmClean baseUrl token ""
+    member __.ScmDelete = Url.scmDelete baseUrl token ""
+    member __.Command = Url.command baseUrl token ""
+    member __.VfsGetFile = Url.vfsGetFile baseUrl token ""
+    member __.VfsPutFile = Url.vfsPutFile baseUrl token ""
+    member __.VfsDeleteFile = Url.vfsDeleteFile baseUrl token ""
+    member __.ZipGet = Url.zipGet baseUrl token ""
+    member __.ZipPut = Url.zipPut baseUrl token ""
+    member __.DeploymentsList = Url.deploymentsList baseUrl token ""
+    member __.DeploymentGet = Url.deploymentGet baseUrl token ""
+    member __.DeploymentPut = Url.deploymentPut baseUrl token ""
+    member __.DeploymentDelete = Url.deploymentDelete baseUrl token ""
+    member __.DeploymentLogGet = Url.deploymentLogGet baseUrl token ""
+    member __.DeploymentLogGetLogId = Url.deploymentLogGetLogId baseUrl token ""
+    member __.DeployPost = Url.deployPost baseUrl token ""
+    member __.SshGetGenertate = Url.sshGetGenertate baseUrl token ""
+    member __.SshGet = Url.sshGet baseUrl token ""
+    member __.SshGetPublicKey = Url.sshGetPublicKey baseUrl token ""
+    member __.KuduVersion = Url.kuduVersion baseUrl token ""
+    member __.SettingsPost = Url.settingsPost baseUrl token ""
+    member __.SettingsGet = Url.settingsGet baseUrl token ""
+    member __.SettingsGetKey = Url.settingsGetKey baseUrl token ""
+    member __.SettingsDeleteKey = Url.settingsDeleteKey baseUrl token ""
+    member __.Diagnostics = Url.diagnostics baseUrl token ""
+    member __.DiagnosticsSettingsPost = Url.diagnosticsSettingsPost baseUrl token ""
+    member __.DiagnosticsSettingsGet = Url.diagnosticsSettingsGet baseUrl token ""
+    member __.DiagnosticsSettingsGetKey = Url.diagnosticsSettingsGetKey baseUrl token ""
+    member __.DiagnosticsSettingsDeleteKey = Url.diagnosticsSettingsDeleteKey baseUrl token ""
+    member __.Logs = Url.logs baseUrl token ""
+    member __.ListExtentionFeed = Url.listExtentionFeed baseUrl token ""
+    member __.ListSiteExtentions = Url.listSiteExtentions baseUrl token ""
+    member __.ListExtentionFeedId = Url.listExtentionFeedId baseUrl token ""
+    member __.ListSiteExtentionsId = Url.listSiteExtentionsId baseUrl token ""
+    member __.InstallSiteExtention = Url.installSiteExtention baseUrl token ""
+    member __.ListExtentions = Url.listExtentions baseUrl token ""
---
+++ RelUrl.fs
@@ -0,0 +1,83 @@
+﻿module internal Fake.Azure.KuduRest.Url
+
+open Fake.Azure.KuduRest.Http
+
+type HttpResult =
+    | OK of string
+    | Error of string * string
+
+let siteRoot siteName =
+    sprintf "https://%s.scm.azurewebsites.net" siteName
+
+let scmInfo baseUrl = 
+    Get (sprintf "%s/api/scm/info" baseUrl)
+let scmClean baseUrl = 
+    Post (sprintf "%s/api/scm/info" baseUrl)
+let scmDelete baseUrl = 
+    Delete (sprintf "%s/api/scm" baseUrl)
+let command baseUrl =
+    Post (sprintf "%s/api/command" baseUrl)
+let vfsGetFile baseUrl path = 
+    Get (sprintf "%s/api/vfs/%s" baseUrl path)
+let vfsPutFile baseUrl path = 
+    Put (sprintf "%s/api/vfs/%s" baseUrl path)
+let vfsDeleteFile baseUrl path = 
+    Delete (sprintf "%s/api/vfs/%s" baseUrl path)
+let zipGet baseUrl path =
+    Get (sprintf "%s/api/zip/%s" baseUrl path)
+let zipPut baseUrl path = 
+    Put (sprintf "%s/api/zip/%s" baseUrl path)
+let deploymentsList baseUrl = 
+    Get (sprintf "%s/api/deployments" baseUrl)
+let deploymentGet baseUrl deploymentId = 
+    Get (sprintf "%s/api/deployments/%s" baseUrl deploymentId)
+let deploymentPut baseUrl deploymentId =
+    Put (sprintf "%s/api/deployments/%s" baseUrl deploymentId)
+let deploymentDelete baseUrl deploymentId =
+    Delete (sprintf "%s/api/deployments/%s" baseUrl deploymentId)
+let deploymentLogGet baseUrl deploymentId =
+    Get (sprintf "%s/api/deployments/%s/log" baseUrl deploymentId)
+let deploymentLogGetLogId baseUrl deploymentId logId =
+    Get (sprintf "%s/api/deployments/%s/log/%s" baseUrl deploymentId logId)
+let deployPost baseUrl =
+    Post (sprintf "%s/deploy" baseUrl)
+let sshGetGenertate baseUrl = 
+    Get (sprintf "%s/api/sshkey?ensurePublicKey=1" baseUrl)
+let sshGet baseUrl = 
+    Put (sprintf "%s/api/sshkey" baseUrl)
+let sshGetPublicKey baseUrl = 
+    Get (sprintf "%s/api/sshkey" baseUrl)
+let kuduVersion baseUrl = 
+    Get (sprintf "%s/api/environment" baseUrl)
+let settingsPost baseUrl = 
+    Post (sprintf "%s/api/settings" baseUrl)
+let settingsGet baseUrl = 
+    Get (sprintf "%s/api/settings" baseUrl)
+let settingsGetKey baseUrl key = 
+    Get (sprintf "%s/api/settings/%s" baseUrl key)
+let settingsDeleteKey baseUrl key =
+    Delete (sprintf "%s/api/settings/%s" baseUrl key)
+let diagnostics baseUrl =
+    Get (sprintf "%s/api/dump" baseUrl)
+let diagnosticsSettingsPost baseUrl =
+    Post (sprintf "%s/api/diagnostics/settings" baseUrl)
+let diagnosticsSettingsGet baseUrl =
+    Get (sprintf "%s/api/diagnostics/settings" baseUrl)
+let diagnosticsSettingsGetKey baseUrl key =
+    Get (sprintf "%s/api/diagnostics/settings/%s" baseUrl key)
+let diagnosticsSettingsDeleteKey baseUrl key =
+    Delete (sprintf "%s/api/diagnostics/settings/%s" baseUrl key)
+let logs baseUrl =
+    Get (sprintf "%s/api/logs/recent" baseUrl)
+let listExtentionFeed baseUrl =
+    Get (sprintf "%s/api/extensionfeed" baseUrl)
+let listSiteExtentions baseUrl =
+    Get (sprintf "%s/api/siteextensions" baseUrl)
+let listExtentionFeedId baseUrl id =
+    Get (sprintf "%s/api/extensionfeed/%s" baseUrl id)
+let listSiteExtentionsId baseUrl id =
+    Get (sprintf "%s/api/siteextensions/%s" baseUrl id)
+let installSiteExtention baseUrl id =
+    Put (sprintf "%s/api/siteextensions/%s" baseUrl id)
+let listExtentions baseUrl id =
+    Delete (sprintf "%s/api/siteextensions/%s" baseUrl id)
