8e786d37c78d79f77991aca1c50fd655c3463b36
Mark Broadhurst <Mark.Broadhurst@gmail.com>
2017-02-09 16:58:55 +0000

    Got working

--- src/Http.fs
+++ src/Http.fs
@@ -1,8 +1,9 @@
-﻿module internal Fake.Azure.KuduRest.Http
+﻿module Fake.Azure.KuduRest.Http
 
 open System
 open System.Text
 open System.Net
+open System.IO
 
 type HttpResult =
     | OK of string
@@ -14,36 +15,40 @@ let private CreateToken username password =
     |> Convert.ToBase64String
     |> sprintf "Basic %s"
 
+let unpackResponse (response:WebResponse) =
+    let httpResponse = response :?> HttpWebResponse
+    use stream = response.GetResponseStream()
+    use reader = new StreamReader(stream)
+    let content = reader.ReadToEnd()
+    if httpResponse.StatusCode = HttpStatusCode.OK
+    then OK(content)
+    else Error(httpResponse.StatusCode.ToString(), content)
 
 let private httpAction token (verb:string) (url:string) =
-    async {
-        let request = WebRequest.CreateHttp(url);
-        request.Headers.Add("Authorization", token)
-        request.Method <- verb
-        return! request.AsyncGetResponse()
-    }
+    let request = WebRequest.CreateHttp(url);
+    request.Headers.Add("Authorization", token)
+    request.Method <- verb
+    request.GetResponse()
+    |> unpackResponse
 
 let private httpActionWithBody token (verb:string) (url:string) (content:string) =
-    async {
-        let request = WebRequest.CreateHttp(url);
-        request.Headers.Add("Authorization", token)
-        request.Method <- verb
-        let stream = request.GetRequestStream()
-        let bytes = Encoding.ASCII.GetBytes(content)
-        stream.Write(bytes,0,bytes.Length)
-        return! request.AsyncGetResponse()
-    }
+    let request = WebRequest.CreateHttp(url);
+    request.Headers.Add("Authorization", token)
+    request.Method <- verb
+    let stream = request.GetRequestStream()
+    let bytes = Encoding.ASCII.GetBytes(content)
+    stream.Write(bytes,0,bytes.Length)
+    request.GetResponse()
+    |> unpackResponse
 
 let private httpActionBinary token (verb:string) (url:string) (bytes:byte[]) =
-    async {
-        let request = WebRequest.CreateHttp(url);
-        request.Headers.Add("Authorization", token)
-        request.Method <- verb
-        let stream = request.GetRequestStream()
-        stream.Write(bytes,0,bytes.Length)
-        return! request.AsyncGetResponse()
-    }
-
+    let request = WebRequest.CreateHttp(url);
+    request.Headers.Add("Authorization", token)
+    request.Method <- verb
+    let stream = request.GetRequestStream()
+    stream.Write(bytes,0,bytes.Length)
+    request.GetResponse()
+    |> unpackResponse
 
 type RestClient(username, password) =
     let token = CreateToken username password
