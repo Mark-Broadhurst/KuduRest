dcff1bbf27e9a6f1d33e9304ad7806b1dfa77a54
Mark Broadhurst <Mark.Broadhurst@gmail.com>
2017-02-10 16:24:19 +0000

    Back to working

--- src/Http.fs
+++ src/Http.fs
@@ -5,41 +5,42 @@ open System.Text
 open System.Net
 open System.IO
 
-type HttpResult =
-    | OK of string
-    | Error of string * string
-
 let private CreateToken username password = 
     sprintf "%s:%s" username password
     |> Encoding.ASCII.GetBytes
     |> Convert.ToBase64String
     |> sprintf "Basic %s"
 
-let unpackResponse (response:WebResponse) =
-    let httpResponse = response :?> HttpWebResponse
-    use stream = response.GetResponseStream()
+let streamToString (stream:Stream) = 
     use reader = new StreamReader(stream)
-    let content = reader.ReadToEnd()
+    reader.ReadToEnd()
+
+let streamToBytes (stream:Stream) =
+    use ms = new MemoryStream()
+    stream.CopyTo(ms);
+    ms.ToArray()
+
+let unpackResponse (unpack: Stream -> 'A) (response:WebResponse) =
+    let httpResponse = response :?> HttpWebResponse
+    let stream = httpResponse.GetResponseStream()
     if httpResponse.StatusCode = HttpStatusCode.OK
-    then OK(content)
-    else Error(httpResponse.StatusCode.ToString(), content)
+    then Choice1Of2(unpack stream)
+    else Choice2Of2(httpResponse.StatusCode.ToString(), streamToString stream)
 
 let private httpAction token (verb:string) (url:string) =
-    let request = WebRequest.CreateHttp(url);
+    let request = WebRequest.CreateHttp(url)
     request.Headers.Add("Authorization", token)
     request.Method <- verb
     request.GetResponse()
-    |> unpackResponse
 
 let private httpActionWithBody token (verb:string) (url:string) (content:string) =
-    let request = WebRequest.CreateHttp(url);
+    let request = WebRequest.CreateHttp(url)
     request.Headers.Add("Authorization", token)
     request.Method <- verb
     let stream = request.GetRequestStream()
     let bytes = Encoding.ASCII.GetBytes(content)
     stream.Write(bytes,0,bytes.Length)
     request.GetResponse()
-    |> unpackResponse
 
 let private httpActionBinary token (verb:string) (url:string) (bytes:byte[]) =
     let request = WebRequest.CreateHttp(url);
@@ -48,7 +49,6 @@ let private httpActionBinary token (verb:string) (url:string) (bytes:byte[]) =
     let stream = request.GetRequestStream()
     stream.Write(bytes,0,bytes.Length)
     request.GetResponse()
-    |> unpackResponse
 
 type RestClient(username, password) =
     let token = CreateToken username password
